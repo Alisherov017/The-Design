59f37f62d2e899d3676e066bd94dc2ed0ce71bd1
azmtbkova <albinaazamatbekova@gmail.com>
2024-07-20 12:16:00 +0500

    footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
