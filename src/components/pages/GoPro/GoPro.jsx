8421d49b02675402db72071a6bd1d436d6edc557
azmtbkova <albinaazamatbekova@gmail.com>
2024-07-23 09:35:45 +0500

    pages

--- src/components/pages/GoPro/GoPro.jsx
+++ src/components/pages/GoPro/GoPro.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styles from "./GoPro.module.css";
 
 const GoPro = () => {
-  return <div>GoPro</div>;
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.protitle}>DRIBBBLE PRO</h3>
+
+      <h1 className={styles.title}>Grow your design business</h1>
+      <p className={styles.subtitle}>
+        Get more eyes on your work and stand out with attention grabbing profile
+        <br /> features to attract more opportunities.
+      </p>
+      <p className={styles.description}>
+        $8/mo <br></br>
+        <button className={styles.button}>Get started today</button>
+      </p>
+    </div>
+  );
 };
 
 export default GoPro;
--- src/components/routes/MainRoutes.jsx
+++ src/components/routes/MainRoutes.jsx
@@ -2,13 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import Designers from "../pages/designers/Designers";
 import Error from "../error/Error";
 import MainLayout from "./MainLayout";
+import Jobs from "../pages/Jobss/Job";
+import GoPro from "../pages/GoPro/GoPro";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    id: "root",
-    errorElement: <Error />,
     element: <MainLayout />,
-    children: [{ path: "/designers", element: <Designers /> }],
+    errorElement: <Error />,
+    children: [
+      { path: "designers", element: <Designers /> },
+      { path: "jobs", element: <Jobs /> },
+      { path: "gopro", element: <GoPro /> },
+    ],
   },
 ]);
+
+export default router;
